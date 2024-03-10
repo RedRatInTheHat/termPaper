1e91db9c98583aac1b30bc729741e85dad369fdb
Pesheva Irina <pesheva.i@yandex.ru>
2024-03-10 10:53:50 +0300

    Add target group

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,6 +110,20 @@ resource "yandex_compute_instance" "nginx-vm-2" {
   }
 }
 
+resource "yandex_alb_target_group" "nginx-tg" {
+  name           = "nginx-target-group"
+
+  target {
+    subnet_id    = yandex_vpc_subnet.subnet-1.id
+    ip_address   = yandex_compute_instance.nginx-vm-1.network_interface.0.ip_address
+  }
+
+  target {
+    subnet_id    = yandex_vpc_subnet.subnet-2.id
+    ip_address   = yandex_compute_instance.nginx-vm-2.network_interface.0.ip_address
+  }
+}
+
 #===prometheus===
 /*resource "yandex_compute_disk" "prometheus-bd" {
   name     = "prometheus-boot-disk"
