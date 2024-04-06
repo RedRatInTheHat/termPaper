ccec50632155b0160111b7b7da16ea3cc492ba5e
Pesheva Irina <pesheva.i@yandex.ru>
2024-04-06 19:57:57 +0300

    #15: add instand and scheduled snapshots

--- terraform/main.tf
+++ terraform/main.tf
@@ -274,13 +274,17 @@ resource "yandex_alb_target_group" "nginx-tg" {
 
   target {
     subnet_id    = yandex_vpc_subnet.private-subnet-1.id
-    ip_address   = yandex_compute_instance.nginx-vm-1.network_interface.0.ip_address
+    ip_address   = "${var.nginx_1_ip}"
   }
 
   target {
     subnet_id    = yandex_vpc_subnet.private-subnet-2.id
-    ip_address   = yandex_compute_instance.nginx-vm-2.network_interface.0.ip_address
+    ip_address   = "${var.nginx_2_ip}"
   }
+
+  depends_on = [
+    yandex_compute_instance.nginx-vm-1, yandex_compute_instance.nginx-vm-2
+  ]
 }
 
 resource "yandex_alb_backend_group" "nginx-bg" {
@@ -551,6 +555,72 @@ resource "yandex_compute_instance" "bastion-vm" {
   scheduling_policy {
     preemptible = var.preemptible
   }
+}
+
+#=====snapshots=====
+#===snapshot schedule====
+resource "yandex_compute_snapshot_schedule" "default" {
+  name           = "all-disks-snapshots"
+
+  schedule_policy {
+	  expression = "0 0 * * *"
+  }
+
+  snapshot_count = 7
+
+  disk_ids = [yandex_compute_disk.nginx-bd-1.id, yandex_compute_disk.nginx-bd-2.id, yandex_compute_disk.prometheus-bd.id, 
+  yandex_compute_disk.grafana-bd.id, yandex_compute_disk.elastic-bd.id, yandex_compute_disk.kibana-bd.id, 
+  yandex_compute_disk.bastion-bd.id]
+}
+
+#======single snapshot=======
+resource "yandex_compute_snapshot" "nginx-snapshot-1" {
+  name           = "nginx-snapshot-1"
+  source_disk_id = yandex_compute_disk.nginx-bd-1.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "nginx-snapshot-2" {
+  name           = "nginx-snapshot-2"
+  source_disk_id = yandex_compute_disk.nginx-bd-2.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "prometheus-snapshot" {
+  name           = "prometheus-snapshot"
+  source_disk_id = yandex_compute_disk.prometheus-bd.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "grafana-snapshot" {
+  name           = "grafana-snapshot"
+  source_disk_id = yandex_compute_disk.grafana-bd.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "elastic-snapshot" {
+  name           = "elastic-snapshot"
+  source_disk_id = yandex_compute_disk.elastic-bd.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "kibana-snapshot" {
+  name           = "kibana-snapshot"
+  source_disk_id = yandex_compute_disk.kibana-bd.id
+
+  depends_on = [ null_resource.ansible ]
+}
+
+resource "yandex_compute_snapshot" "bastion-snapshot" {
+  name           = "bastion-snapshot"
+  source_disk_id = yandex_compute_disk.bastion-bd.id
+
+  depends_on = [ null_resource.ansible ]
 }
 
 #===ansible info====
